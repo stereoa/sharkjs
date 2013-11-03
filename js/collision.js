6afa5a2a19b55a3478c3f9d415019be2c6c69ff1
stereoa <stereotypeaura@gmail.com>
2013-11-03 16:38:01 -0500

    seperated collisions to diff js

---
+++ js/collision.js
@@ -0,0 +1,38 @@
+function crewMemberHitsCrewMember(crewMember, crewMemberVictim) {
+
+    if (crewMember.name == "bomb" || crewMemberVictim.name == "bomb") {
+        if (crewMember.y > waterLine + 50 && crewMember.y > waterLine + 50) {
+
+            if (crewMember.name =="bomb") createExplosion(crewMember.x, crewMember.y);
+            else createExplosion(crewMemberVictim.x, crewMemberVictim.y);
+            crewMember.kill();
+            crewMemberVictim.kill();
+
+            changeScore(50);
+        }
+    }
+}
+
+function sharkHitsPerson(shark, crewObject) {
+    if (crewObject.name == 'bomb') {
+        createExplosion(crewObject.x,crewObject.y);
+        stunShark();
+        crewObject.kill();
+    }
+    else {
+        if (!isStunned) {
+            crewObject.kill();
+            changeScore(1);
+        }
+    }
+}
+
+function sharkHitsBoat(shark, boat) {
+    if (!isStunned) {
+        if (shark.y < 26) shark.body.velocity.x *= -.4;
+
+        shark.body.velocity.y = 20;
+        boatSpawn(randomNum(1, 4));
+        stunShark();
+    }
+}
--- js/game.js
+++ js/game.js
@@ -78,49 +78,12 @@ function changeScore(changeAmount) {
     score += changeAmount;
     txtScore.setText(score.toString());
 }
-function crewMemberHitsCrewMember(crewMember, crewMemberVictim) {
 
-    if (crewMember.name == "bomb" || crewMemberVictim.name == "bomb") {
-        if (crewMember.y > waterLine + 50 && crewMember.y > waterLine + 50) {
-
-            if (crewMember.name =="bomb") createExplosion(crewMember.x, crewMember.y);
-            else createExplosion(crewMemberVictim.x, crewMemberVictim.y);
-            crewMember.kill();
-            crewMemberVictim.kill();
-
-            changeScore(50);
-        }
-    }
-}
 function createExplosion(x,y){
     var explosionAnimation = explosions.getFirstDead();
     explosionAnimation.reset(x,y);
     explosionAnimation.play('kaboom', 30, false, true);
 }
-function sharkHitsPerson(shark, crewObject) {
-    if (crewObject.name == 'bomb') {
-        createExplosion(crewObject.x,crewObject.y);
-        stunShark();
-        crewObject.kill();
-    }
-    else {
-        if (!isStunned) {
-            crewObject.kill();
-            changeScore(1);
-        }
-    }
-}
-
-function sharkHitsBoat(shark, boat) {
-    if (!isStunned) {
-        if (shark.y < 26) shark.body.velocity.x *= -.4;
-
-        shark.body.velocity.y = 20;
-        boatSpawn(randomNum(1, 4));
-        stunShark();
-    }
-}
-
 function handleNPCs() {
     //controls the boat movements
     handleBoat();
